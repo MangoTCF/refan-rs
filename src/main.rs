139617cd702daa2a4465a5ca61f00e51ea4ec522
Mango <mango.tcf@gmail.com>
2024-08-05 13:08:15 +1000

    Gitignore changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::{
-    io::{Read, Write},
+    io::{Read, Seek, Write},
     time::Duration,
 };
 
 use anyhow::Context;
 use clap::Parser;
-use log::info;
-use simplelog::{TermLogger, TerminalMode};
+use log::{debug, info};
+use simplelog::{trace, TermLogger, TerminalMode};
 use structs::{Args, Config, TPoint};
 
 mod structs;
@@ -19,34 +19,38 @@ fn map<T: num_traits::Num + Copy>(x: T, in_min: T, in_max: T, out_min: T, out_ma
 
 fn main() {
     let args = Args::parse();
-    TermLogger::new(
+    TermLogger::init(
         args.verbosity.to_level_filter(),
         simplelog::Config::default(),
         TerminalMode::Mixed,
         simplelog::ColorChoice::Auto,
-    );
+    ).unwrap();
     info!(target: "main", "ReFan v{} online!", VERSION);
     let mut cfg: Config =
-        toml::from_str(std::fs::read_to_string(args.config_path).unwrap().as_str()).unwrap();
+        toml::from_str(std::fs::read_to_string(args.config_path).context("Reading the configuration file").unwrap().as_str()).unwrap();
     for fan in &mut cfg.fans {
-        fan.pwm_mode_path
+        fan.pwm_mode
             .write(b"1")
             .context(format!("Trying to set mode for fan {}", fan.name))
             .unwrap();
         log::info!("{} online!", fan.name);
     }
+    if args.dt_override != -1.0 {
+        cfg.dt = args.dt_override;
+    }
     loop {
         std::thread::sleep(Duration::from_secs_f32(cfg.dt));
         for fan in &mut cfg.fans {
+            fan.temp_sensor.seek(std::io::SeekFrom::Start(0)).unwrap();
             //read and parse the temperature
-            let mut buf = String::default();
-            fan.temp_sensor_path
-                .read_to_string(&mut buf)
+            let mut buf = [0;32];
+            let len = fan.temp_sensor
+                .read(&mut buf)
                 .context(format!("Reading temperature for fan {}", fan.name))
                 .unwrap();
-            let t = buf
+            let t = core::str::from_utf8(&buf[0..len]).unwrap()
                 .parse::<f32>()
-                .context(format!("Parsing temperature for fan {}", fan.name))
+                .context(format!("Parsing temperature for fan {} from {} of len {}", fan.name, core::str::from_utf8(&buf).unwrap(), len))
                 .unwrap()
                 / 1000f32;
 
@@ -79,16 +83,17 @@ fn main() {
                 tpoint_l.pwm as f32,
                 tpoint_h.pwm as f32,
             )
-            .clamp(fan.pwm_min as f32, fan.pwm_max as f32) as i16;
-            if fan.stopped && pwm > fan.pwm_start {
+            .clamp(fan.pwm_min as f32, fan.pwm_max as f32);
+            if fan.stopped && (pwm as i16) > fan.pwm_start {
                 fan.stopped = false;
             }
-            if (!fan.stopped) && pwm < fan.pwm_stop {
+            if (!fan.stopped) && (pwm as i16) < fan.pwm_stop {
                 fan.stopped = true;
             }
+            debug!(target: fan.name.as_str(), "Stopped: {}, pwm: {}, temp: {}, lerp between {:?} and {:?}", fan.stopped, pwm, t, tpoint_l, tpoint_h);
             if !fan.stopped {
-                fan.pwm_write_path
-                    .write(pwm.to_string().as_bytes())
+                fan.pwm_write
+                    .write((pwm as i16).to_string().as_bytes())
                     .context(format!("Writing PWM value {} for fan {}", pwm, fan.name))
                     .unwrap();
             }
--- src/structs.rs
+++ src/structs.rs
@@ -1,6 +1,7 @@
 use std::fs::{File, OpenOptions};
 
 use clap::Parser;
+use glob::glob;
 use log::Level;
 use serde::{Deserialize, Deserializer};
 
@@ -8,22 +9,54 @@ fn deserialize_path_to_file_ro<'de, D>(deserializer: D) -> Result<File, D::Error
 where
     D: Deserializer<'de>,
 {
+    let path = String::deserialize(deserializer).unwrap();
+
+    if path.contains("*") {
+        let path = glob(&path)
+            .expect("Invalid pattern given")
+            .next()
+            .unwrap()
+            .unwrap();
+        return Ok(OpenOptions::new()
+            .write(false)
+            .read(true)
+            .create(false)
+            .open(path)
+            .unwrap());
+    }
+
     return Ok(OpenOptions::new()
         .write(false)
         .read(true)
         .create(false)
-        .open(String::deserialize(deserializer).unwrap())
+        .open(path)
         .unwrap());
 }
 fn deserialize_path_to_file_rw<'de, D>(deserializer: D) -> Result<File, D::Error>
 where
     D: Deserializer<'de>,
 {
+    let path = String::deserialize(deserializer).unwrap();
+
+    if path.contains("*") {
+        let path = glob(&path)
+            .expect("Invalid pattern given")
+            .next()
+            .unwrap()
+            .unwrap();
+        return Ok(OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(false)
+            .open(path)
+            .unwrap());
+    }
+
     return Ok(OpenOptions::new()
         .write(true)
         .read(true)
         .create(false)
-        .open(String::deserialize(deserializer).unwrap())
+        .open(path)
         .unwrap());
 }
 
@@ -32,10 +65,16 @@ where
 #[command(version, about, long_about = None)]
 pub struct Args {
     //Path to refan.toml
+    #[arg(short, long, default_value = "/etc/refan.toml")]
     pub config_path: String,
 
     //Verbosity
+    #[arg(short, long, default_value = "INFO")]
     pub verbosity: Level,
+
+    //Override time between updates
+    #[arg(short = 't', long, default_value = "-1")]
+    pub dt_override: f32,
 }
 
 #[derive(Deserialize)]
@@ -46,22 +85,32 @@ pub struct Config {
 
 #[derive(Deserialize)]
 pub struct Fan {
-    #[serde(deserialize_with = "deserialize_path_to_file_ro")]
-    pub temp_sensor_path: File,
-    #[serde(deserialize_with = "deserialize_path_to_file_rw")]
-    pub pwm_write_path: File,
-    #[serde(deserialize_with = "deserialize_path_to_file_rw")]
-    pub pwm_mode_path: File,
+    #[serde(
+        deserialize_with = "deserialize_path_to_file_ro",
+        rename = "temp_sensor_path"
+    )]
+    pub temp_sensor: File,
+    #[serde(
+        deserialize_with = "deserialize_path_to_file_rw",
+        rename = "pwm_write_path"
+    )]
+    pub pwm_write: File,
+    #[serde(
+        deserialize_with = "deserialize_path_to_file_rw",
+        rename = "pwm_mode_path"
+    )]
+    pub pwm_mode: File,
     pub curve: Vec<TPoint>,
     pub pwm_start: i16,
     pub pwm_stop: i16,
+    #[serde(skip_deserializing)]
     pub stopped: bool,
     pub pwm_min: i16,
     pub pwm_max: i16,
     pub name: String,
 }
 
-#[derive(Deserialize, Clone, Copy, Default)]
+#[derive(Deserialize, Clone, Copy, Default, Debug)]
 pub struct TPoint {
     pub t: f32,
     pub pwm: i16,
